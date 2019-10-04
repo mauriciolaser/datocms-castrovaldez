f4672aa29bd8293b96530759e277a0241525aabd
Mauricio CV <mauricio@rayolaser.pe>
2019-10-03 20:59:37 -0500

    initial commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -48,7 +48,7 @@ const TemplateWrapper = ({ children }) => (
       <div className="container__sidebar">
         <div className="sidebar">
           <h6 className="sidebar__title">
-            <Link to="/">{data.datoCmsSite.globalSeo.siteName}</Link>
+            <a href="https://docs.google.com/document/d/1O-M3HPytPWUrOtbjGhkhkfg_nwuS7fzJkSWXTVIkXLk/edit?usp=sharing">{data.datoCmsSite.globalSeo.siteName}</a>
           </h6>
           <div
             className="sidebar__intro"
